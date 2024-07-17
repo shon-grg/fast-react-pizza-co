8e6e8fe5a204d5600a0dc6a1b84b02cbf074206b
Shon George <shongeorge2000@gmail.com>
2024-07-17 15:48:03 +0530

    Initial Commit

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,7 @@
+function Username() {
+  return (
+    <div className="hidden text-sm font-semibold md:block ">Shone George</div>
+  );
+}
+
+export default Username;
---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types, no-unused-vars */
+
+import Header from './Header';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigate } from 'react-router-dom';
+import Loader from './Loader';
+
+function AppLayout() {
+  const navigation = useNavigate();
+  const isLoading = navigation.state === 'loading';
+
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
+
+      <Header />
+      <div className=" overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
